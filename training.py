3233c09abd0d99c1e650dded5d096c8eaaca629d
empty300 <empty300@mail.ru>
2023-02-15 21:27:35 +0800

    Решено несколько задач

--- training.py
+++ training.py
@@ -122,3 +122,4 @@ test.assert_equals(queue_time([2], 5), 2, "wrong answer for a single person in t
 test.assert_equals(queue_time([1, 2, 3, 4, 5], 1), 15, "wrong answer for a single till")
 test.assert_equals(queue_time([1, 2, 3, 4, 5], 100), 5, "wrong answer for a case with a large number of tills")
 test.assert_equals(queue_time([2, 2, 3, 3, 4, 4], 2), 9, "wrong answer for a case with two tills")
+1
