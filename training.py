fe5833839beb0fb7fbc526e170d04883beee94e6
empty300 <empty300@mail.ru>
2023-03-15 20:21:44 +0800

    Решено несколько задач

--- training.py
+++ training.py
@@ -11,7 +11,10 @@ number in base 10. This may be True and False in your language, e.g. PHP.
 Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be
 passed into the function.
 """
+import math
+
 import codewars_test as test
+import numpy as numpy
 
 
 def narcissistic(value):
@@ -122,3 +125,51 @@ test.assert_equals(queue_time([2], 5), 2, "wrong answer for a single person in t
 test.assert_equals(queue_time([1, 2, 3, 4, 5], 1), 15, "wrong answer for a single till")
 test.assert_equals(queue_time([1, 2, 3, 4, 5], 100), 5, "wrong answer for a case with a large number of tills")
 test.assert_equals(queue_time([2, 2, 3, 3, 4, 4], 2), 9, "wrong answer for a case with two tills")
+
+
+"""
+For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, 
+which is a utility class helpful for querying paging information related to an array.
+The class is designed to take in an array of values and an integer indicating how many items will be 
+allowed per each page. The types of values contained within the collection/array are not relevant.
+"""
+
+class PaginationHelper:
+
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+
+    def item_count(self):
+        return len(self.collection)
+
+    def page_count(self):
+        return math.ceil(len(self.collection) / self.items_per_page)
+
+    def page_item_count(self, page_index):
+
+        if page_index > self.page_count() - 1:
+            return -1
+        elif page_index == self.page_count() - 1:
+            return self.item_count() - page_index * self.items_per_page
+        else:
+            return self.items_per_page
+
+    def page_index(self, item_index):
+        page = 0
+        if self.item_count() - 1 < item_index or item_index < 0:
+            return -1
+        else:
+            while item_index > self.items_per_page:
+                item_index -= self.items_per_page
+                page += 1
+        return page
+
+
+collection = range(1, 25)
+helper = PaginationHelper(collection, 10)
+
+test.assert_equals(helper.page_count(), 3, 'page_count is returning incorrect value.')
+test.assert_equals(helper.page_index(23), 2, 'page_index returned incorrect value')
+test.assert_equals(helper.item_count(), 24, 'item_count returned incorrect value')
+test.assert_equals(helper.page_item_count(2), 4, 'item_count returned incorrect value')
